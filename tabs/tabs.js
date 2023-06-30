c96b6adc5fd05d5397122931bedba4b3bbb49571
Leighton <leim56@hawaii.rr.com>
2023-06-30 04:16:47 -1000

    organized folders, finished a lot of front end

---
+++ tabs/tabs.js
@@ -0,0 +1,16 @@
+// Get the tab buttons and tab content
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContent = document.querySelectorAll('.tab-content div');
+
+// Add event listener to each tab button
+tabButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // Remove 'active' class from all buttons and hide all tab content
+    tabButtons.forEach((btn) => btn.classList.remove('active'));
+    tabContent.forEach((content) => (content.style.display = 'none'));
+
+    // Add 'active' class to the clicked button and display the corresponding content
+    button.classList.add('active');
+    tabContent[index].style.display = 'block';
+  });
+});
